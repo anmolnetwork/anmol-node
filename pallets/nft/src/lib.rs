b2544662c5bef65752cc41d6a7beddd4e997c652
korzewski <korzewski@gmail.com>
2021-05-24 14:39:55 +0200

    Added NftPendingQueue and offchain worker trigger

--- pallets/nft/src/lib.rs
+++ pallets/nft/src/lib.rs
@@ -18,6 +18,13 @@ mod tests;
 pub type Dna = Vec<u8>;
 pub type Cid = Vec<u8>;
 
+#[derive(Encode, Decode, Clone, PartialEq, Eq, RuntimeDebug, Default)]
+pub struct PendingNft<AccountId, ClassId> {
+	account_id: AccountId,
+	class_id: ClassId,
+	token_data: TokenData,
+}
+
 #[cfg_attr(feature = "std", derive(Serialize, Deserialize))]
 #[derive(Encode, Decode, Clone, PartialEq, Eq, RuntimeDebug, Default)]
 pub struct ClassData {
@@ -25,7 +32,7 @@ pub struct ClassData {
 }
 
 #[cfg_attr(feature = "std", derive(Serialize, Deserialize))]
-#[derive(Encode, Decode, Clone, PartialEq, Eq, RuntimeDebug)]
+#[derive(Encode, Decode, Clone, PartialEq, Eq, RuntimeDebug, Default)]
 pub struct TokenData {
 	dna: Dna,
 	// To be expanded
@@ -52,16 +59,17 @@ pub mod pallet {
 	#[pallet::generate_store(pub(super) trait Store)]
 	pub struct Pallet<T>(_);
 
+	#[pallet::storage]
+	pub(super) type NftPendingQueue<T: Config> = StorageValue<_, Vec<PendingNft<T::AccountId, T::ClassId>>, ValueQuery>;
+
 	#[pallet::event]
 	#[pallet::metadata(T::AccountId = "AccountId")]
 	#[pallet::generate_deposit(pub(super) fn deposit_event)]
 	pub enum Event<T: Config> {
 		NftClassCreated(T::AccountId, T::ClassId, Cid, ClassData),
+		NftRequest(PendingNft<T::AccountId, T::ClassId>),
 		NftMinted(T::AccountId, T::ClassId, T::TokenId, Cid, TokenData),
 	}
-
-	#[pallet::hooks]
-	impl<T: Config> Hooks<BlockNumberFor<T>> for Pallet<T> {}
 
 	#[pallet::call]
 	impl<T:Config> Pallet<T> {
@@ -76,6 +84,26 @@ pub mod pallet {
 			Ok(().into())
 		}
 
+		#[pallet::weight(10_000 + T::DbWeight::get().reads_writes(1, 1))]
+		pub fn nft_request(origin: OriginFor<T>, class_id: T::ClassId, token_data: TokenData) -> DispatchResultWithPostInfo {
+			let account_id = ensure_signed(origin)?;
+
+			// TODO: Check DNA if valid and unique
+
+			let pending_nft = PendingNft {
+				account_id,
+				class_id,
+				token_data,
+			};
+
+			NftPendingQueue::<T>::mutate(|pending_nft_queue| {
+				pending_nft_queue.push(pending_nft.clone());
+			});
+
+			Self::deposit_event(Event::NftRequest(pending_nft));
+			Ok(().into())
+		}
+
 		#[pallet::weight(10_000 + T::DbWeight::get().reads_writes(2, 4))]
 		pub fn mint_nft(origin: OriginFor<T>, class_id: T::ClassId, metadata: Cid) -> DispatchResultWithPostInfo {
 			let account_id = ensure_signed(origin)?;
@@ -86,6 +114,23 @@ pub mod pallet {
 
 			Self::deposit_event(Event::NftMinted(account_id, class_id, token_id, metadata, token_data));
 			Ok(().into())
+		}
+	}
+
+	#[pallet::hooks]
+	impl<T: Config> Hooks<BlockNumberFor<T>> for Pallet<T> {
+		fn offchain_worker(_block_number: T::BlockNumber) {
+			let pending_nft_queue = NftPendingQueue::<T>::get();
+			if pending_nft_queue.len() > 0 {
+				Self::execute_nft_from_pending_queue(pending_nft_queue[0].clone());
+			}
+		}
+	}
+
+	impl<T:Config> Pallet<T> {
+		fn execute_nft_from_pending_queue(pending_nft: PendingNft<T::AccountId, T::ClassId>) {
+			debug::RuntimeLogger::init();
+			debug::info!("--- execute_nft_from_pending_queue(): {:?}", pending_nft);
 		}
 	}
 }
