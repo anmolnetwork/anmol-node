468e0cd8a011386392edcd4dd485cf6a50f0c20e
korzewski <korzewski@gmail.com>
2021-05-25 13:37:37 +0200

    Add a unique dna validation

--- pallets/nft/src/lib.rs
+++ pallets/nft/src/lib.rs
@@ -1,11 +1,13 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 
 pub use pallet::*;
-use frame_support::{dispatch::DispatchResultWithPostInfo, pallet_prelude::*};
+use frame_support::{
+	dispatch::DispatchResultWithPostInfo, pallet_prelude::*,
+	storage::{IterableStorageDoubleMap},
+};
 use frame_system::pallet_prelude::*;
 #[cfg(feature = "std")]
 use serde::{Serialize, Deserialize};
-use sp_std::vec;
 use sp_std::vec::Vec;
 use orml_nft::Module as OrmlNft;
 
@@ -25,6 +27,8 @@ pub struct PendingNft<AccountId, ClassId> {
 	token_data: TokenData,
 }
 
+pub type PendingNftOf<T> = PendingNft<<T as frame_system::Config>::AccountId, <T as orml_nft::Config>::ClassId>;
+
 #[cfg_attr(feature = "std", derive(Serialize, Deserialize))]
 #[derive(Encode, Decode, Clone, PartialEq, Eq, RuntimeDebug, Default)]
 pub struct ClassData {
@@ -36,14 +40,6 @@ pub struct ClassData {
 pub struct TokenData {
 	dna: Dna,
 	// To be expanded
-}
-
-impl TokenData {
-	fn new(dna: Dna) -> Self {
-		TokenData {
-			dna,
-		}
-	}
 }
 
 #[frame_support::pallet]
@@ -60,15 +56,15 @@ pub mod pallet {
 	pub struct Pallet<T>(_);
 
 	#[pallet::storage]
-	pub(super) type NftPendingQueue<T: Config> = StorageValue<_, Vec<PendingNft<T::AccountId, T::ClassId>>, ValueQuery>;
+	pub(super) type NftPendingQueue<T: Config> = StorageValue<_, Vec<PendingNftOf<T>>, ValueQuery>;
 
 	#[pallet::event]
 	#[pallet::metadata(T::AccountId = "AccountId")]
 	#[pallet::generate_deposit(pub(super) fn deposit_event)]
 	pub enum Event<T: Config> {
-		NftClassCreated(T::AccountId, T::ClassId, Cid, ClassData),
-		NftRequest(PendingNft<T::AccountId, T::ClassId>),
-		NftMinted(T::AccountId, T::ClassId, T::TokenId, Cid, TokenData),
+		NftClassCreated(T::AccountId, T::ClassId, ClassData, Cid),
+		NftRequest(PendingNftOf<T>),
+		NftMinted(T::TokenId, Cid, PendingNftOf<T>),
 	}
 
 	#[pallet::call]
@@ -80,15 +76,13 @@ pub mod pallet {
 			let class_data = ClassData{}; // TODO: To be expanded
 			let class_id = OrmlNft::<T>::create_class(&account_id, metadata.clone(), class_data.clone())?;
 
-			Self::deposit_event(Event::NftClassCreated(account_id, class_id, metadata, class_data));
+			Self::deposit_event(Event::NftClassCreated(account_id, class_id, class_data, metadata));
 			Ok(().into())
 		}
 
 		#[pallet::weight(10_000 + T::DbWeight::get().reads_writes(1, 1))]
 		pub fn nft_request(origin: OriginFor<T>, class_id: T::ClassId, token_data: TokenData) -> DispatchResultWithPostInfo {
 			let account_id = ensure_signed(origin)?;
-
-			// TODO: Check DNA if valid and unique
 
 			let pending_nft = PendingNft {
 				account_id,
@@ -105,14 +99,17 @@ pub mod pallet {
 		}
 
 		#[pallet::weight(10_000 + T::DbWeight::get().reads_writes(2, 4))]
-		pub fn mint_nft(origin: OriginFor<T>, class_id: T::ClassId, metadata: Cid) -> DispatchResultWithPostInfo {
-			let account_id = ensure_signed(origin)?;
+		pub fn root_mint_nft(origin: OriginFor<T>, metadata: Cid, pending_nft: PendingNftOf<T>) -> DispatchResultWithPostInfo {
+			let _ = ensure_root(origin)?;
 
-			let dna = vec![0, 1, 2]; // TODO: Generic DNA
-			let token_data = TokenData::new(dna);
-			let token_id = OrmlNft::<T>::mint(&account_id, class_id.clone(), metadata.clone(), token_data.clone())?;
+			let token_id = OrmlNft::<T>::mint(
+				&pending_nft.account_id,
+				pending_nft.class_id.clone(),
+				metadata.clone(),
+				pending_nft.token_data.clone(),
+			)?;
 
-			Self::deposit_event(Event::NftMinted(account_id, class_id, token_id, metadata, token_data));
+			Self::deposit_event(Event::NftMinted(token_id, metadata, pending_nft));
 			Ok(().into())
 		}
 	}
@@ -128,9 +125,29 @@ pub mod pallet {
 	}
 
 	impl<T:Config> Pallet<T> {
-		fn execute_nft_from_pending_queue(pending_nft: PendingNft<T::AccountId, T::ClassId>) {
+		fn execute_nft_from_pending_queue(pending_nft: PendingNftOf<T>) {
 			debug::RuntimeLogger::init();
 			debug::info!("--- execute_nft_from_pending_queue(): {:?}", pending_nft);
+
+			let mut tokens_iterator = <orml_nft::Tokens<T> as IterableStorageDoubleMap<T::ClassId, T::TokenId, orml_nft::TokenInfoOf<T>>>
+				::iter_prefix(pending_nft.class_id);
+
+			let mut unique_dna = true;
+			while let Some((token_id, token_info)) = tokens_iterator.next() {
+				debug::info!("--- token_id: {:?}, token_info: {:?}", token_id, token_info);
+				
+				if pending_nft.token_data.dna == token_info.data.dna {
+					unique_dna = false;
+					break;
+				}
+			}
+
+			if !unique_dna {
+				debug::info!("--- Not a unique dna: {:?}", pending_nft.token_data.dna);
+				return
+			}
+			
+			debug::info!("--- Create a new NFT on-chain");
 		}
 	}
 }
