ebbd2c03de7e44ea15fe385f523a5a76b29f3696
korzewski <korzewski@gmail.com>
2021-06-03 15:08:57 +0200

    feat: offchain storage poc

--- pallets/nft/src/lib.rs
+++ pallets/nft/src/lib.rs
@@ -14,11 +14,18 @@ use serde::{Serialize, Deserialize};
 use sp_std::{
 	vec::Vec,
 	cmp::{Ordering},
+	str,
+	fmt,
 };
 use sp_core::{
 	crypto::KeyTypeId,
 };
-use sp_runtime::{DispatchError};
+use sp_runtime::{
+	DispatchError,
+	offchain::{
+		storage::{StorageValueRef},
+	},
+};
 use orml_nft::Module as OrmlNft;
 
 #[cfg(test)]
@@ -93,6 +100,20 @@ impl TokenData {
 	}
 }
 
+struct OffchainStorageKey(Vec<u8>);
+
+impl fmt::LowerHex for OffchainStorageKey {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        for b in &self.0 {
+            write!(f, "{:02x}", b)?;
+        }
+        Ok(())
+    }
+}
+
+const LOCAL_PENDING_NFT_QUEUE: &[u8] = b"nft_pending_queue";
+const LOCAL_NFT_FROM_BLOCK_PREFIX: &[u8] = b"nft_from_block_";
+
 #[frame_support::pallet]
 pub mod pallet {
 	use super::*;
@@ -119,6 +140,10 @@ pub mod pallet {
 		NoLocalAccountForSigning,
 		OffchainSignedTxError,
 		TryToRemoveNftWhichDoesNotExist,
+		OffchainLock,
+		OffchainValueNotFound,
+		OffchainValueDecode,
+		OffchainValueMutate,
 	}
 
 	#[pallet::event]
@@ -221,15 +246,100 @@ pub mod pallet {
 
 	#[pallet::hooks]
 	impl<T: Config> Hooks<BlockNumberFor<T>> for Pallet<T> {
-		fn offchain_worker(_block_number: T::BlockNumber) {
+		fn on_finalize(block_number: T::BlockNumber) {
+			let key = Self::get_offchain_new_nft_items_key(block_number);
 			let pending_nft_queue = NftPendingQueue::<T>::get();
-			if pending_nft_queue.len() > 0 {
-				Self::execute_nft_from_pending_queue(pending_nft_queue[0].clone());
+
+			sp_io::offchain_index::set(&key.0, &pending_nft_queue.encode());
+			
+			debug::info!("--- on_finalize block_number: {:?}, key: {:x}, value: {:?}", block_number, key, pending_nft_queue);
+			
+			NftPendingQueue::<T>::put(Vec::<PendingNftOf<T>>::new());
+		}
+
+		fn offchain_worker(block_number: T::BlockNumber) {
+			debug::info!("--- offchain_worker block_number: {:?}", block_number);
+
+			// let key = Self::get_offchain_new_nft_items_key(block_number);
+			// let local_data: Vec<PendingNftOf::<T>> = match StorageValueRef::persistent(&key.0).get() {
+			// 	Some(Some(x)) => x,
+			// 	Some(None) => {
+			// 		debug::info!("--- error OffchainValueDecode");
+			// 		Vec::<PendingNftOf<T>>::new()
+			// 	},
+			// 	None => {
+			// 		debug::info!("--- error OffchainValueNotFound");
+			// 		Vec::<PendingNftOf<T>>::new()
+			// 	}
+			// };
+			// debug::info!("--- offchain_worker key: {:x} value: {:?}", key, local_data);
+
+
+
+			let result = Self::offchain_update_local_pending_nft_queue(block_number);
+
+			match result {
+				Ok(pending_nft_queue) => {
+					if pending_nft_queue.len() > 0 {
+						debug::info!("--- execute pending_nft_queue: {:?}", pending_nft_queue);
+						Self::execute_nft_from_pending_queue(pending_nft_queue[0].clone());
+					}
+				},
+				Err(x) => {
+					debug::error!("--- result error: {:?}", x);
+				}
 			}
 		}
 	}
 
 	impl<T:Config> Pallet<T> {
+		fn get_offchain_new_nft_items_key(block_number: T::BlockNumber) -> OffchainStorageKey {
+			let mut key = LOCAL_NFT_FROM_BLOCK_PREFIX.to_vec();
+			key.extend(block_number.encode());
+			OffchainStorageKey(key)
+		}
+
+		fn get_offchain_new_nft_items(block_number: T::BlockNumber) -> Result<Vec<PendingNftOf<T>>, Error<T>>{
+			let key = Self::get_offchain_new_nft_items_key(block_number);
+			let new_nft_items = StorageValueRef::persistent(&key.0)
+				.get::<Vec<PendingNftOf<T>>>()
+				.ok_or(Error::<T>::OffchainValueNotFound)?
+				.ok_or(Error::<T>::OffchainValueDecode)?;
+
+			debug::info!("--- get_offchain_new_nft_items key: {:x}, new_nft_items: {:?}", key, new_nft_items);
+
+			Ok(new_nft_items)
+		}
+
+		fn offchain_update_local_pending_nft_queue(block_number: T::BlockNumber) -> Result<Vec<PendingNftOf<T>>, Error<T>> {
+			let value_ref = StorageValueRef::persistent(LOCAL_PENDING_NFT_QUEUE);
+			let result = value_ref.mutate(|x: Option<Option<Vec<PendingNftOf<T>>>>| {
+				match x {
+					Some(Some(mut current_pending_nft_queue)) => {
+						debug::info!("--- current_pending_nft_queue: {:?}", current_pending_nft_queue);
+						let new_nft_items = Self::get_offchain_new_nft_items(block_number)?;
+						for v in new_nft_items {
+							current_pending_nft_queue.push(v);
+						}
+						
+						Ok(current_pending_nft_queue)
+					},
+					_ => {
+						debug::info!("--- current_pending_nft_queue: EMPTY");
+						Ok(Vec::<PendingNftOf<T>>::new())
+					},
+				}
+			});
+
+			match result {
+				Ok(Ok(pending_nft_queue)) => {
+					Ok(pending_nft_queue)
+				},
+				Err(x) => Err(x),
+				Ok(Err(_)) => Err(Error::<T>::OffchainLock),
+			}
+		}
+
 		fn execute_nft_from_pending_queue(pending_nft: PendingNftOf<T>) {
 			debug::RuntimeLogger::init();
 			debug::info!("--- Execute nft from pending queue: {:?}", pending_nft);
