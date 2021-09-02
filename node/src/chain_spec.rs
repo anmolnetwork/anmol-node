ea626a93de8f485022de9f12f4fea6c6a01a7281
Justin <justinfrevert@gmail.com>
2021-09-01 23:24:38 -0400

    feat: ibtida genesis config

--- node/src/chain_spec.rs
+++ node/src/chain_spec.rs
@@ -1,12 +1,15 @@
 use anmol_runtime::{
-	AccountId, AuraConfig, BalancesConfig, BaseNftConfig, GenesisConfig, GrandpaConfig, Signature,
-	SudoConfig, SystemConfig, WASM_BINARY,
+	AccountId, AuraConfig, Balance, BalancesConfig, BaseNftConfig, GenesisConfig, GrandpaConfig,
+	SudoConfig, SystemConfig, WASM_BINARY, Signature, constants::tokens::TOKEN_COUNT
 };
 use sc_service::{ChainType, Properties};
 use sp_consensus_aura::sr25519::AuthorityId as AuraId;
 use sp_core::{sr25519, Pair, Public};
 use sp_finality_grandpa::AuthorityId as GrandpaId;
-use sp_runtime::traits::{IdentifyAccount, Verify};
+use sp_runtime::traits::{Verify, IdentifyAccount};
+use sp_runtime::AccountId32;
+// use hex_literal;
+use hex_literal::hex;
 
 // The URL for the telemetry server.
 // const STAGING_TELEMETRY_URL: &str = "wss://telemetry.polkadot.io/submit/";
@@ -43,6 +46,92 @@ pub fn chain_properties() -> Properties {
 	properties.insert("ss58Format".into(), 64.into());
 	properties
 }
+
+/// Configure initial storage state for FRAME modules.
+fn testnet_genesis(
+	wasm_binary: &[u8],
+	initial_authorities: Vec<(AuraId, GrandpaId)>,
+	root_key: AccountId,
+	endowed_accounts: Vec<AccountId>,
+	_enable_println: bool,
+) -> GenesisConfig {
+	// base nft class for genesis block
+	let initial_state = vec![(
+		get_account_id_from_seed::<sr25519::Public>("Alice"),
+		[0].to_vec(),
+		(),
+		[].to_vec(),
+	)];
+
+	GenesisConfig {
+		frame_system: Some(SystemConfig {
+			// Add Wasm runtime to storage.
+			code: wasm_binary.to_vec(),
+			changes_trie_config: Default::default(),
+		}),
+		pallet_balances: Some(BalancesConfig {
+			// Configure endowed accounts with initial balance of 1 << 60.
+			balances: endowed_accounts.iter().cloned().map(|k|(k, 1 << 60)).collect(),
+		}),
+		pallet_aura: Some(AuraConfig {
+			authorities: initial_authorities.iter().map(|x| (x.0.clone())).collect(),
+		}),
+		pallet_grandpa: Some(GrandpaConfig {
+			authorities: initial_authorities.iter().map(|x| (x.1.clone(), 1)).collect(),
+		}),
+		pallet_sudo: Some(SudoConfig {
+			// Assign network admin rights.
+			key: root_key,
+		}),
+		base_nft: Some(BaseNftConfig {
+			tokens: initial_state,
+		}),
+	}
+}
+
+fn ibtida_genesis(
+	wasm_binary: &[u8],
+	initial_authorities: Vec<(AuraId, GrandpaId)>,
+	root_key: AccountId32,
+	founder_allocation:  Vec<(AccountId32, Balance)>,
+	_enable_println: bool,
+) -> GenesisConfig {
+	// base nft class for genesis block
+	let initial_state = vec![(
+		get_account_id_from_seed::<sr25519::Public>("Alice"),
+		[0].to_vec(),
+		(),
+		[].to_vec(),
+	)];
+
+	GenesisConfig {
+		frame_system: Some(SystemConfig {
+			// Add Wasm runtime to storage.
+			code: wasm_binary.to_vec(),
+			changes_trie_config: Default::default(),
+		}),
+		pallet_balances: Some(BalancesConfig {
+			balances: founder_allocation
+				.iter()
+				.map(|x| (x.0.clone(), x.1.clone()))
+				.collect(),
+		}),
+		pallet_grandpa: Some(GrandpaConfig {
+			authorities: initial_authorities.iter().map(|x| (x.1.clone(), 1)).collect(),
+		}),		
+		pallet_aura: Some(AuraConfig {
+			authorities: initial_authorities.iter().map(|x| (x.0.clone())).collect(),
+		}),
+		pallet_sudo: Some(SudoConfig {
+			// Assign network admin rights.
+			key: root_key,
+		}),
+		base_nft: Some(BaseNftConfig {
+			tokens: initial_state,
+		}),
+	}
+}
+
 
 pub fn development_config() -> Result<ChainSpec, String> {
 	let wasm_binary = WASM_BINARY.ok_or_else(|| "Development wasm not available".to_string())?;
@@ -133,51 +222,44 @@ pub fn local_testnet_config() -> Result<ChainSpec, String> {
 	))
 }
 
-/// Configure initial storage state for FRAME modules.
-fn testnet_genesis(
-	wasm_binary: &[u8],
-	initial_authorities: Vec<(AuraId, GrandpaId)>,
-	root_key: AccountId,
-	endowed_accounts: Vec<AccountId>,
-	_enable_println: bool,
-) -> GenesisConfig {
-	// base nft class for genesis block
-	let initial_state = vec![(
-		get_account_id_from_seed::<sr25519::Public>("Alice"),
-		[0].to_vec(),
-		(),
-		[].to_vec(),
-	)];
+// testnet config
+pub fn ibtida_config() -> Result<ChainSpec, String> {
+	let wasm_binary = WASM_BINARY.ok_or_else(|| "Development wasm not available".to_string())?;
+	let anmol_ibtida_faucet: AccountId32 = hex!["169af6d461e12d74fe252c853c8728bb4bed714af3a2bc70406a95e4b471bb43"].into();
 
-	GenesisConfig {
-		frame_system: Some(SystemConfig {
-			// Add Wasm runtime to storage.
-			code: wasm_binary.to_vec(),
-			changes_trie_config: Default::default(),
-		}),
-		pallet_balances: Some(BalancesConfig {
-			// Configure endowed accounts with initial balance of 1 << 60.
-			balances: endowed_accounts
-				.iter()
-				.cloned()
-				.map(|k| (k, 1 << 60))
-				.collect(),
-		}),
-		pallet_aura: Some(AuraConfig {
-			authorities: initial_authorities.iter().map(|x| (x.0.clone())).collect(),
-		}),
-		pallet_grandpa: Some(GrandpaConfig {
-			authorities: initial_authorities
-				.iter()
-				.map(|x| (x.1.clone(), 1))
-				.collect(),
-		}),
-		pallet_sudo: Some(SudoConfig {
-			// Assign network admin rights.
-			key: root_key,
-		}),
-		base_nft: Some(BaseNftConfig {
-			tokens: initial_state,
-		}),
-	}
+	Ok(ChainSpec::from_genesis(
+		// Name
+		"Ibtida",
+		// ID
+		"ibtida",
+		ChainType::Live,
+		move || {
+			ibtida_genesis(
+				wasm_binary,
+				// Initial PoA authorities
+				vec![
+					authority_keys_from_seed(&anmol_ibtida_faucet.to_string()),
+				],
+				// Sudo account
+				
+				anmol_ibtida_faucet.clone(),
+				// Pre-funded accounts
+				vec![
+				// Issue #1
+					(anmol_ibtida_faucet.clone(), TOKEN_COUNT)
+				],
+				true,
+			)
+		},
+		// Bootnodes
+		vec![],
+		// Telemetry
+		None,
+		// Protocol ID
+		None,
+		// Properties
+		Some(chain_properties()),
+		// Extensions
+		None,
+	))
 }
