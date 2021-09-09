6e47bc334a4079176e558686a326df0b9cf24014
Justin <justinfrevert@gmail.com>
2021-09-09 00:01:38 -0400

    refactor: fix EOF and remove unused module import

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -75,8 +75,6 @@ pub type Hash = sp_core::H256;
 /// Digest item type.
 pub type DigestItem = generic::DigestItem<Hash>;
 
-pub mod constants;
-
 /// Opaque types. These are used by the CLI to instantiate machinery that don't need to know
 /// the specifics of the runtime. They can then be made to be agnostic over specific formats
 /// of data like extrinsics, allowing for them to continue syncing the network through upgrades
